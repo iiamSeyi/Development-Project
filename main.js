6454f9b2411dd9f1b8fd8a7b19e2fc49609ffe51
iiamSeyi <oluwaseyisalisu@gmail.com>
2024-04-03 14:36:20 -0400

    relative

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import * as THREE from '/three';
+import { GLTFLoader } from '/three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from '/three/addons/controls/OrbitControls.js';
 import WebGL from 'three/addons/capabilities/WebGL.js';
 
 if ( WebGL.isWebGLAvailable() ) {
