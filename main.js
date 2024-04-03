63b4b1702adc917a2d5ddf5f6a1fc40b9ebcf261
iiamSeyi <oluwaseyisalisu@gmail.com>
2024-04-03 14:05:53 -0400

    rotation commit

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
@@ -24,8 +25,9 @@ loader.load(
 
 		scene.add( gltf.scene );
 
-        model.rotation.x = Math.PI / 8; 
-        model.rotation.y = Math.PI / 4;
+        gltf.rotation.x = Math.PI / 8; 
+        gltf.rotation.y = Math.PI / 4;
+        mgltf.rotation.z = Math.PI / 10;
 
 		gltf.animations; // Array<THREE.AnimationClip>
 		gltf.scene; // THREE.Group
@@ -50,7 +52,13 @@ loader.load(
 
 camera.position.z = 120;
 camera.position.x = 20;
-camera.position.y = 30;
+camera.position.y = 100;
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true; 
+controls.dampingFactor = 0.25;
+controls.screenSpacePanning = false;
+controls.maxPolarAngle = Math.PI / 2;
 
 function animate() {
     requestAnimationFrame(animate);
